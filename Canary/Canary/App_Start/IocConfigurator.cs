b8dc21b1786c497a144d141ae7d21548690f07b2
ask-imran <s.askar.imran@gmail.com>
2017-09-15 11:08:08 +0530

    Id:003 Added unity container, owin

---
+++ Canary/Canary/App_Start/IocConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+
+namespace Canary.App_Start
+{
+    public class IocConfigurator
+    {
+        public static void Configure()
+        {
+            IUnityContainer _unityContainer = new UnityContainer();
+            RegisterDependencies(_unityContainer);
+        }
+        private static void RegisterDependencies(IUnityContainer _unityContainer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
