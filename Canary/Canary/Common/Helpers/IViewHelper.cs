22aabc832c81bcf258a6a05a015302c93cbc500b
ask-imran <s.askar.imran@gmail.com>
2017-09-19 11:58:42 +0530

    Id:006 Added Layout.cshtml and ViewHelper -Added basic menu

---
+++ Canary/Canary/Common/Helpers/IViewHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canary.Common.Helpers
+{
+    public interface IViewHelper
+    {
+        string GetLayoutPath(string layoutFileName = null);
+    }
+}
---
+++ Canary/Canary/Common/Helpers/ViewHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Canary.Common.Helpers
+{
+    public class ViewHelper : IViewHelper
+    {
+        private const string DefaultLayoutFileName = "_Layout.cshtml";
+        public string GetLayoutPath(string layoutFileName = null)
+        {
+            if (!string.IsNullOrWhiteSpace(layoutFileName))
+            {
+                return string.Format("~/Views/{0}", layoutFileName);
+            }
+            return string.Format("~/Views/{0}", DefaultLayoutFileName);
+        }
+    }
+}
