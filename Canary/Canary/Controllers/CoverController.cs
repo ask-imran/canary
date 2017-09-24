d4c11ec01cdf6babded11b770080cf1b6604dd00
ask-imran <s.askar.imran@gmail.com>
2017-09-24 20:48:23 +0530

    -Added Cover page -folder Restructured

--- Canary/Canary/Controllers/CoverController.cs
+++ Canary/Canary/Controllers/CoverController.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Canary.Controllers
 {
-    public class HomeController : SiteBaseController
+    public class CoverController : Controller
     {
+        //
+        // GET: /Cover/
         public ActionResult Index()
         {
             return View();
-
         }
-    }
+	}
 }
